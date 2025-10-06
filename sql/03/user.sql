c04c8fadfb84997739342022d56ba055e9d06353
Finnvdh01 <39246@ma-web.nl>
2025-10-06 10:44:24 +0200

    commit 6

--- sql/03/user.sql
+++ sql/03/user.sql
@@ -3,3 +3,6 @@ FLUSH PRIVILEGES;
 
 revoke select ON m5prog.* From 'm5progphp'@'%';
 FLUSH PRIVILEGES;
+
+Grant Select, delete, insert, update, execute ON m5prog.* to 'm5progphp'@'%';
+FLUSH PRIVILEGES;
